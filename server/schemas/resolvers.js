41d5e7f53b6d0cd461f6937f10808db314c74c95
Tifni Shroll <tasshroll@gmail.com>
2023-08-01 21:20:38 -0700

    addUser added in resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -42,6 +42,22 @@ const resolvers = {
             const token = signToken(user);
             return { token, user }
         },
+        saveArticle: async (parent, { articleData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    {
+                        $addToSet:
+                            { savedArticles: articleData }
+                    },
+                    { new: true }
+                );
+                return updatedUser;
+
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
 
     },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,8 @@ const { gql } = require('apollo-server-express');
 
 // these typeDefs match the mongo models in ./models
 // these are the objects that are returned when a user makes a query
+// ArticleInfo is used to pass data into our saveArticle mutation
+
 const typeDefs = gql`
 
 type User {
@@ -25,6 +27,14 @@ type Auth {
     user : User
     }
 
+input ArticleInfo {
+    description : String, 
+    title : String, 
+    articleId : String, 
+    image: String, 
+    link: String
+    }
+
 type Query {
     me: User
     }
@@ -32,7 +42,7 @@ type Query {
 type Mutation {
     login (email : String, password : String) : Auth
     addUser(username : String, email: String, password: String) : Auth
-    saveArticle(articleId : ID, description : String, title : String, image : String, link : String) : User
+    saveArticle(articleData: ArticleInfo!): User
     removeArticle(articleId : ID) : User
     }
 `;
