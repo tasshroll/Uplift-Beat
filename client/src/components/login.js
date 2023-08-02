be44c0dc9ee121a6b4219b3ae2f5a11887ace9b3
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-01 19:22:08 -0700

    adding login return

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -40,4 +40,18 @@ const handleFormSubmit = async (event) => {
       username: '',
       password: '',
     });
+
+    return (
+        <div>
+          <input
+             type="text"
+             value={"loginForm"}
+             placeholder="Enter a message"
+             onChange={e => setMessage(e.target.value)}
+           />
+          <p>
+            <strong>{message}</strong>
+          </p>
+        </div>
+      );
 };
