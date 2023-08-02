6722142e25ec6419048540cbc5f6061eadc85ea3
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-01 18:52:12 -0700

    using react for login instead

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,69 +1,14 @@
 import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
 
-const LoginForm = () => {
+import React from 'react';
 
-};
-
-const loginForm = document.querySelector("#login");
-const signupForm = document.querySelector("#signup");
-
-loginForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const email = document.querySelector("#login-email").value;
-  const password = document.querySelector("#login-password").value;
-  const data = {
-    email,
-    password,
-  };
-
-  fetch("/api/users/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      if (response.ok) {
-        window.location.href = "/";
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
-
-signupForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const name = document.querySelector("#name").value;
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-  const rePassword = document.querySelector("#re-password").value;
-  const phoneNumber = document.querySelector("#phone-number").value;
-
-  const data = {
-    name,
-    email,
-    password,
-    rePassword,
-    phoneNumber,
-  };
-
-  fetch("/api/users/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      if (response.ok) {
-        window.location.href = "/";
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      list: [],    
+    };
+  }
+  /* ... */
+}
