b005018c589e66075df3f972c16eba33b0d74cb7
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-01 19:32:48 -0700

    perfecting code with chatgpt

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,57 +1,60 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-const loginForm = () => {
-    const loginInfo = useState({ username: '', password: '' });
-    const validated = useState(false);
-    const loginAlert = useState(false);
-};
+const LoginForm = () => {
+    const [loginInfo, setLoginInfo] = useState({ email: '', password: '' });
+    const [validated, setValidated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
 
-const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...loginInfo, [name]: value });
-};
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setLoginInfo(prevState => ({ ...prevState, [name]: value }));
+    };
 
-const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+            setValidated(true);
+        } else {
+            try {
+                const response = await loginUser(loginInfo);
+      
+                if (!response.ok) {
+                  throw new Error('there was an error logging you in');
+                }
+      
+                const { token, user } = await response.json();
+                console.log(user);
+                Auth.login(token);
 
-    try {
-        const response = await loginUser(loginInfo);
-  
-        if (!response.ok) {
-          throw new Error('there was an error logging you in');
+                setLoginInfo({ email: '', password: '' });
+                setShowAlert(false);
+            } catch (err) {
+                console.error(err);
+                setShowAlert(true);
+            }
         }
-  
-        const { token, user } = await response.json();
-        console.log(user);
-        Auth.login(token);
-      } catch (err) {
-        console.error(err);
-        setShowAlert(true);
-      }
-
-    setUserFormData({
-      username: '',
-      password: '',
-    });
+    };
 
     return (
-        <div>
-          <input
-             type="text"
-             value={"loginForm"}
-             placeholder="Enter a message"
-             onChange={e => setMessage(e.target.value)}
-           />
-          <p>
-            <strong>{message}</strong>
-          </p>
-        </div>
-      );
+        <>
+          <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+              Something went wrong with your login credentials!
+            </Alert>
+            {/* Form elements... */}
+            <Button
+            disabled={!(userFormData.email && userFormData.password)}
+            type='submit'
+            variant='success'>
+            Submit
+            </Button>
+          </Form>
+        </>
+    );
 };
+
+export default LoginForm;
