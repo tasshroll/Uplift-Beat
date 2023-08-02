81c79c637661e919d4502276c88bff278af69720
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-01 19:03:39 -0700

    adding to the login - adding data saving features

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,4 +2,25 @@ import React, { useState } from 'react';
 
 const loginForm = () => {
     const loginInfo = useState({ username: '', password: '' });
- }
+};
+
+const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+};
+
+const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+};
