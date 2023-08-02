efb11a174ec777a4132add24c26e1e4397440d89
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-01 19:10:42 -0700

    login information response notif added

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 const loginForm = () => {
     const loginInfo = useState({ username: '', password: '' });
+    const validated = useState(false);
+    const loginAlert = useState(false);
 };
 
 const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    setUserFormData({ ...loginInfo, [name]: value });
 };
 
 const handleFormSubmit = async (event) => {
@@ -17,6 +19,21 @@ const handleFormSubmit = async (event) => {
       event.preventDefault();
       event.stopPropagation();
     }
+
+    try {
+        const response = await loginUser(loginInfo);
+  
+        if (!response.ok) {
+          throw new Error('there was an error logging you in');
+        }
+  
+        const { token, user } = await response.json();
+        console.log(user);
+        Auth.login(token);
+      } catch (err) {
+        console.error(err);
+        setShowAlert(true);
+      }
 
     setUserFormData({
       username: '',
