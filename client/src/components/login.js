7397ca418d4ed1fa920b9f50c7c780add1521a48
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-01 19:39:50 -0700

    added more functionality and username -> email

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -45,13 +45,40 @@ const LoginForm = () => {
             <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
               Something went wrong with your login credentials!
             </Alert>
-            {/* Form elements... */}
+
+            <Form.Group className='mb-3'>
+                <Form.Label htmlFor='email'>Email</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Your email'
+                    name='email'
+                    onChange={handleInputChange}
+                    value={loginInfo.email}
+                    required
+                />
+                <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label htmlFor='password'>Password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Your password'
+                    name='password'
+                    onChange={handleInputChange}
+                    value={loginInfo.password}
+                    required
+                />
+                <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+            </Form.Group>
+
             <Button
-            disabled={!(userFormData.email && userFormData.password)}
-            type='submit'
-            variant='success'>
-            Submit
+                disabled={!(loginInfo.email && loginInfo.password)}
+                type='submit'
+                variant='success'>
+                Submit
             </Button>
+
           </Form>
         </>
     );
