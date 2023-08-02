a42347449ae86367adc1c313ec2e95b0483777a4
Mario Inzunza <marioelgallo92@gmail.com>
2023-08-01 19:49:58 -0700

    carousel function

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UncontrolledExample from './UncontrolledExample';
+
+function App() {
+  return (
+    <div>
+      <UncontrolledExample />
+    </div>
+  );
+}
+
+export default App;
