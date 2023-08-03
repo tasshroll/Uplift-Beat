e0ab654ef47146cc5e151855a26af998226d010b
Tifni Shroll <tasshroll@gmail.com>
2023-08-02 18:56:40 -0700

    fix argument for removeArticle mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -52,11 +52,11 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
 
         },
-        removeArticle: async (parent, { articleData }, context) => {
+        removeArticle: async (parent, { articleId }, context) => {
             if (context.user) {
             const updatedUser = await User.findByIdAndUpdate(
                 { _id: context.user._id },
-                { $pull: { savedArticles: { articleData } } },
+                { $pull: { savedArticles: { articleId } } },
                 { new: true }
             );
             return updatedUser;
