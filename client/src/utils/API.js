1b1acb860d92d2550b6d7b7a535ce27e2e68bd85
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-03 18:56:22 -0700

    added navbar and moved API call out of sever side

---
+++ client/src/utils/API.js
@@ -0,0 +1,28 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('insertKeyHere');
+
+const router = require('express').Router();
+
+async function fetchTechnologyNewsSources() {
+  try {
+    const response = await newsapi.v2.sources({
+      category: 'technology',
+      language: 'en',
+      country: 'us'
+    });
+    return response;
+  } catch (error) {
+    console.error('Error fetching news sources:', error);
+    return { status: 'error', message: 'Failed to fetch news sources' };
+  }
+}
+
+router.get('/technology-news-sources', async (req, res) => {
+  const newsSources = await fetchTechnologyNewsSources();
+  res.json(newsSources);
+});
+
+const userRoutes = require('./userRoutes');
+router.use('/users', userRoutes);
+
+module.exports = router;
